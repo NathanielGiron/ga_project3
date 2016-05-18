eb746cc4008f3e073918e605758c6156fad656b3
brytree <brycebortree@gmail.com>
2016-05-17 18:56:32 -0700

    emails show propery

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @users = User.all
   end
 
   def create
@@ -12,6 +13,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, :user_id)
   end
 end
