57fe78d6cd459de832391edd1f6432d3cd191f08
brytree <brycebortree@gmail.com>
2016-05-18 14:56:48 -0700

    removed extra config

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,6 @@ module SaasProjectApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += ['messages/create.js']
-
   end
 end
 
