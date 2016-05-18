b21dc1d05071510e20c6c42727c73476ce35f0f5
brytree <brycebortree@gmail.com>
2016-05-18 14:52:15 -0700

    trying precompiling only one page

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,9 @@ module SaasProjectApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assest.precompile += ['messages/create.js']
+
   end
 end
 
-config.assest.precompile += ['messages/create.js']
 
