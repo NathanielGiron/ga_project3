7df72b3aa3b586b03615590a835482053eadfc99
brytree <brycebortree@gmail.com>
2016-05-18 14:55:22 -0700

    compiling only the create file

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module SaasProjectApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assest.precompile += ['messages/create.js']
+    config.assets.precompile += ['messages/create.js']
 
   end
 end
