e11d59b548d89b3b70406d24a4e258ce3f77882b
brytree <brycebortree@gmail.com>
2016-05-19 13:46:46 -0700

    changes to messages

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -7,15 +7,3 @@ $(document).ready(function(){
       location.reload()
     });
 });
-
-
-
-// PrivatePub.subscribe("/messages/new", function(data, channel) {
-//   console.log('last');
-//   $("#chat").append("<%= j render(@message) %>");
-//   $("#new_message")[0].reset();
-//   });
-
-// PrivatePub.subscribe("/messages/new", function(data, channel) {
-//   $("#chat").append(data.chat_message);
-// });
