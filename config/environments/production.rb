f430821ca4371b038f06e6899ddc688a2bc64927
brytree <brycebortree@gmail.com>
2016-05-19 18:55:31 -0700

    changed url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'sharkfile.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'sharkfiler.herokuapp.com', :protocol => 'https'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
